c6d8bd451d1f48f7c65dcc14a11180d4eea865e8
dushyantbhati2 <db90054@gmail.com>
2024-05-28 06:34:28 +0530

    schemas and apis created

--- server/backend/base/models.py
+++ server/backend/base/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from uuid import uuid4
 # Create your models here.
 class Category(models.Model):
-    
     name=models.CharField(max_length=255)
     def __str__(self):
         return self.name
